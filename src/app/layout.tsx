d756dc930d8c55eaf07eb1c2c78483496af0822f
Richi Beja <richibeja@gmail.com>
2026-08-03 19:29:04 -0500

    Install Meta Pixel and track InitiateCheckout and Purchase events

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,8 @@ export const metadata: Metadata = {
   description: "Participa en nuestras rifas y gana increíbles premios. Plataforma 100% segura y automática.",
 };
 
+import Script from "next/script";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -30,6 +32,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" className={`${inter.variable} ${outfit.variable}`}>
+      <head>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '934265183000144');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   );
